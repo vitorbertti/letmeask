3034d83ad65b223c1257fa975c2acd2f067fff1e
Vitor Bertti <berttivitor@gmail.com>
2021-06-22 21:51:19 -0300

    Added context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
-function App() {
+import Home from './pages/Home';
+import NewRoom from './pages/NewRoom';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import AuthContextProvider from './contexts/AuthContext';
+
+const App = () => {
+  
+
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route exact path="/" component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+
   );
 }
 
