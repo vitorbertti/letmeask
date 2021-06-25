952cda99a17c3e61f4d14dedd2f0a9d5036d8cba
Vitor Bertti <berttivitor@gmail.com>
2021-06-24 21:33:47 -0300

    Added send question function

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react';
+import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import Button from '../components/Button';
 
 import RoomCode from '../components/RoomCode';
+import useAuth from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 
@@ -10,30 +14,107 @@ type RoomParams = {
    id: string;
 }
 
+type FirebaseQuestions = Record<string, {
+   author: {
+      name: string;
+      avatar: string;
+   };
+   content: string;
+   isHighlighted: boolean;
+   isAnswered: boolean;
+}>
+
+type Question = {
+   id: string;
+   author: {
+      name: string;
+      avatar: string;
+   };
+   content: string;
+   isHighlighted: boolean;
+   isAnswered: boolean;
+}
+
 const Room = () => {
    const params = useParams<RoomParams>();
+   const roomId = params.id;
+   const [newQuestion, setNewQuestion] = useState('');
+   const { user } = useAuth();
+   const [questions, setQuestions] = useState<Question[]>([]);
+   const [title, setTitle] = useState('');
+
+   useEffect(() => {
+      const roomRef = database.ref(`rooms/${roomId}`);
+
+      roomRef.on('value', room => {
+         const databaseRoom = room.val();
+         const firebaseQuestions: FirebaseQuestions = databaseRoom.question ?? {};
+         const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+            return {
+               id: key,
+               content: value.content,
+               author: value.author,
+               isHighlighted: value.isHighlighted,
+               isAnswered: value.isAnswered,
+            }
+         });
+         setTitle(databaseRoom.title);
+         setQuestions(parsedQuestions);
+      });
+   }, [roomId]);
+
+   const handleSendQuestion = async (e: FormEvent) => {
+      e.preventDefault();
+
+      if(newQuestion.trim() === '') return;
+
+      if(!user) {
+         throw new Error('You must be logged in');
+      }
+
+      const question = {
+         content: newQuestion,
+         author: {
+            name: user.name,
+            avatar: user.avatar,
+         },
+         isHighlighted: false,
+         isAnswered: false,
+      };
+
+      await database.ref(`rooms/${roomId}/questions`).push(question);
+
+      setNewQuestion('');
+   };
 
    return (
       <div id="#page-room">
          <header>
             <div className="content">
                <img src={logoImg} alt="Letmeask" />
-               <RoomCode code={params.id} />
+               <RoomCode code={roomId} />
             </div>
          </header>
 
          <main className="content">
             <div className="room-title">
-               <h1>Sala React</h1>
-               <span>4 perguntas</span>
+               <h1>Sala {title}</h1>
+               { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+               
             </div>
 
-            <form>
-               <textarea placeholder="O que você quer perguntar?"></textarea>
+            <form onSubmit={handleSendQuestion}>
+               <textarea placeholder="O que você quer perguntar?" value={newQuestion} onChange={e => setNewQuestion(e.target.value)} />
 
                <div className="form-footer">
-                  <span>Para enviar uma pergunta, <button>faça seu login</button> .</span>
-                  <Button type="submit">Enviar pergunta</Button>
+                  { user && <div className="user-info">
+                     <img src={user.avatar} alt={user.name} />
+                     <span>{user.name}</span>
+                  </div> }
+
+                  { !user && <span>Para enviar uma pergunta, <button>faça seu login</button> .</span> }
+                  
+                  <Button disabled={!user} type="submit">Enviar pergunta</Button>
                </div>
             </form>
          </main>
